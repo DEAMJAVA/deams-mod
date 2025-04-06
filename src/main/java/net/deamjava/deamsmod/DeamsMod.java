e422ed45dab263cda56623d090b0697cf8840e52
DEAMJAVA <deamminecraft3@gmail.com>
2025-04-06 13:23:32 +0530

    Test Commit

--- src/main/java/net/deamjava/deamsmod/DeamsMod.java
+++ src/main/java/net/deamjava/deamsmod/DeamsMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-//change
+//change in mod
 public class DeamsMod implements ModInitializer {
 	public static final String MOD_ID = "deamsmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
